b29366333e1af38dd76fa4f755a10ac45dddee5d
Brendalu2005 <brenda.luanacb@gmail.com>
2025-04-02 20:38:46 -0300

    iniciando o front-end

--- FocusTime/urls.py
+++ FocusTime/urls.py
@@ -1,6 +1,6 @@
-# from django.urls import path
-# from FocusTime.views import index, teste
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-    
-# ]
+urlpatterns = [
+    path('', views.home)
+]
--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FocusTime.apps.FocusTimeConfig'
+    'FocusTime.apps.FocusTimeConfig',
 ]
 
 MIDDLEWARE = [
