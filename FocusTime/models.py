0ba25690a37d1e269d9f8a138c4ef0f98e3cea2e
Brendalu2005 <brenda.luanacb@gmail.com>
2025-04-03 22:30:23 -0300

    Criação do banco de dados de metas + correções html e views

--- FocusTime/models.py
+++ FocusTime/models.py
@@ -3,12 +3,18 @@ from django.db import models
 
 class Materia(models.Model):
     nome_materia = models.CharField(max_length=100)
-    horas_meta = models.IntegerField()
-    minutos_meta = models.IntegerField()
-    segundos_meta = models.IntegerField()
     horas = models.IntegerField()
     minutos = models.IntegerField()
     segundos = models.IntegerField()
 
     def __str__(self):
         return self.nome_materia
+    
+class Meta(models.Model):
+    nome_metas = models.CharField(max_length=100)
+    horas_meta = models.IntegerField()
+    minutos_meta = models.IntegerField()
+    segundos_meta = models.IntegerField()
+
+    def __str__(self):
+        return self.nome_metas
--- FocusTime/views.py
+++ FocusTime/views.py
@@ -1,37 +1,65 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Materia
+from .models import Meta
 
 # Create your views here.
 
 def index (request):
+    materias = Materia.objects.all()
     return render(request, "index.html")
 
 
 def cadastro (request):
     if request.method == 'POST':
         nome_materia = request.POST.get('nome-materia')
-        horas_meta = request.POST.get('horas-meta')
-        minutos_meta = request.POST.get('minutos-meta')
-        segundos_meta = request.POST.get('segundos-meta')
         horas = request.POST.get('horas')
         minutos = request.POST.get('minutos')
         segundos = request.POST.get('segundos')
+        nome_meta = request.POST.get('nome-meta')
+        horas_meta = request.POST.get('horas-meta')
+        minutos_meta = request.POST.get('minutos-meta')
+        segundos_meta = request.POST.get('segundos-meta')
+        if nome_materia and horas and minutos and segundos and nome_meta and horas_meta and minutos_meta and segundos_meta:
+            materia = Materia(
+                nome_materia=nome_materia,
+                horas=horas,
+                minutos=minutos,
+                segundos=segundos,
+            )
+            meta = Meta(
+                nome_metas=nome_meta,
+                horas_meta=horas_meta,
+                minutos_meta=minutos_meta,
+                segundos_meta=segundos_meta,
+            )
+            meta.save()
+            materia.save()
+            return redirect('index')
+        else:
+            messages.error(request, 'Por favor, preencha todos os campos da matéria.')
+            return render(request, "cadastro.html")
         
-        materia = Materia(
-            nome_materia=nome_materia,
-            horas_meta=horas_meta,
-            minutos_meta=minutos_meta,
-            segundos_meta=segundos_meta,
-            horas=horas,
-            minutos=minutos,
-            segundos=segundos
-        )
-        materia.save()
-    
-        messages.success(request, f'Materia "{nome_materia}" cadastrada!')
-    
-        return redirect('index')
     
     return render (request ,"cadastro.html")
+
+# def CadastroMetas(request):
+#     if request.method == 'POST':
+#         nome_meta = request.POST.get('nome-meta')
+#         horas_meta = request.POST.get('horas-meta')
+#         minutos_meta = request.POST.get('minutos-meta')
+#         segundos_meta = request.POST.get('segundos-meta')
+
+#         if nome_meta and horas_meta and minutos_meta and segundos_meta:
+#             meta = Meta(
+#                 horas_meta=horas_meta,
+#                 minutos_meta=minutos_meta,
+#                 segundos_meta=segundos_meta,
+#             )
+#             meta.save()
+#             return redirect('index')
+#         else:
+#             messages.error(request, 'Por favor, preencha todos os campos da meta.')
+
+#     return render (request ,"cadastro.html")
 
