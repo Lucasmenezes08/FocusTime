04b0916574904a8664b9811e3c8dc1b24995701d
Brendalu2005 <brenda.luanacb@gmail.com>
2025-04-06 14:55:37 -0300

    Correção do banco de dados (valores atualizados corretamente)

--- FocusTime/models.py
+++ FocusTime/models.py
@@ -3,18 +3,18 @@ from django.db import models
 
 class Materia(models.Model):
     nome_materia = models.CharField(max_length=100)
-    horas = models.IntegerField()
-    minutos = models.IntegerField()
-    segundos = models.IntegerField()
+    horas = models.CharField(max_length=10)
+    minutos = models.CharField(max_length=10)
+    segundos = models.CharField(max_length=10)
 
     def __str__(self):
         return self.nome_materia
     
 class Meta(models.Model):
     nome_metas = models.CharField(max_length=100)
-    horas_meta = models.IntegerField()
-    minutos_meta = models.IntegerField()
-    segundos_meta = models.IntegerField()
+    horas_meta = models.CharField(max_length=10)
+    minutos_meta = models.CharField(max_length=10)
+    segundos_meta = models.CharField(max_length=10)
 
     def __str__(self):
         return self.nome_metas
--- FocusTime/views.py
+++ FocusTime/views.py
@@ -12,13 +12,31 @@ def index (request):
 def cadastro (request):
     if request.method == 'POST':
         nome_materia = request.POST.get('nome-materia')
-        horas = request.POST.get('horas')
-        minutos = request.POST.get('minutos')
-        segundos = request.POST.get('segundos')
+        horas1 = request.POST.get('horas')
+        horas2 = request.POST.get('horas2')
+        horas = str(horas1) + str(horas2) if horas1 and horas2 else '00'
+
+        minutos1 = request.POST.get('minutos')
+        minutos2= request.POST.get('minutos2')
+        minutos = str(minutos1) + str(minutos2) if minutos1 and minutos2 else '00'
+
+        segundos1 = request.POST.get('segundos')
+        segundos2 = request.POST.get('segundos2')
+        segundos = str(segundos1) + str(segundos2) if segundos1 and segundos2 else '00'
+
         nome_meta = request.POST.get('nome-meta')
-        horas_meta = request.POST.get('horas-meta')
-        minutos_meta = request.POST.get('minutos-meta')
-        segundos_meta = request.POST.get('segundos-meta')
+        horas_meta1 = request.POST.get('horas-meta')
+        horas_meta2 = request.POST.get('horas-meta2')
+        horas_meta = str(horas_meta1) + str(horas_meta2) if horas_meta1 and horas_meta2 else '00'
+
+        minutos_meta1 = request.POST.get('minutos-meta')
+        minutos_meta2 = request.POST.get('minutos-meta2')
+        minutos_meta = str(minutos_meta1) + str(minutos_meta2) if minutos_meta1 and minutos_meta2 else '00'
+
+        segundos_meta1 = request.POST.get('segundos-meta')
+        segundos_meta2 = request.POST.get('segundos-meta2')
+        segundos_meta = str(segundos_meta1) + str(segundos_meta2) if segundos_meta1 and segundos_meta2 else '00'
+
         if nome_materia and horas and minutos and segundos and nome_meta and horas_meta and minutos_meta and segundos_meta:
             materia = Materia(
                 nome_materia=nome_materia,
@@ -51,24 +69,4 @@ def cronometro (request):
         "metas": metas
     }
     return render (request,"cronometro.html" , contexto)
-
-# def CadastroMetas(request):
-#     if request.method == 'POST':
-#         nome_meta = request.POST.get('nome-meta')
-#         horas_meta = request.POST.get('horas-meta')
-#         minutos_meta = request.POST.get('minutos-meta')
-#         segundos_meta = request.POST.get('segundos-meta')
-
-#         if nome_meta and horas_meta and minutos_meta and segundos_meta:
-#             meta = Meta(
-#                 horas_meta=horas_meta,
-#                 minutos_meta=minutos_meta,
-#                 segundos_meta=segundos_meta,
-#             )
-#             meta.save()
-#             return redirect('index')
-#         else:
-#             messages.error(request, 'Por favor, preencha todos os campos da meta.')
-
-#     return render (request ,"cadastro.html")
 
