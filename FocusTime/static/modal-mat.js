5f68e4e39bda09074100aa8548947caaadbbe819
Lucasmenezes08 <lms4@cesar.school>
2025-04-24 10:46:08 -0300

    :sparkles: feat: Implementar modal de visualização de matérias. closes #10

--- FocusTime/static/modal-mat.js
+++ FocusTime/static/modal-mat.js
@@ -1,15 +1,15 @@
-document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('click', () => {
+document.querySelectorAll('.btn-consultar-materia').forEach(button => {
+  button.addEventListener('click', () => {
       const id = button.dataset.id;
       const modal = document.getElementById(`modal-${id}`);
-      if (modal) modal.style.display = 'flex';
-    });
+      if (modal) modal.classList.add('show');
   });
+});
 
-  document.querySelectorAll('.btn-close-modal').forEach(btn => {
-    btn.addEventListener('click', () => {
+document.querySelectorAll('.btn-close-modal').forEach(btn => {
+  btn.addEventListener('click', () => {
       const id = btn.dataset.id;
       const modal = document.getElementById(`modal-${id}`);
-      if (modal) modal.style.display = 'none';
-    });
+      if (modal) modal.classList.remove('show');
   });
+})
